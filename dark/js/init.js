619f0fad0e3668452c1bbdc23f28bb213de84771
L i [z] u M <lizumebrahim@gmail.com>
2024-10-30 10:57:53 +0500

    Add files via upload

---
+++ dark/js/init.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+						   
+	$("#countdown").countdown({
+				date: "14 jan 2025 09:00:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+
+});
---
+++ light/js/init.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+						   
+	$("#countdown").countdown({
+				date: "14 jan 2025 18:00:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+
+});
